2bb972d28582f2c8322efd34d2af227ae1aec574
Jekman <jeka_zzz@mail.ru>
2020-02-25 18:22:00 +0300

    feat: resolve carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,16 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+const LN2 = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(val) {
+  if (typeof val !== 'string') return false;
+
+  let parsedVal = parseFloat(val);
+
+  if (parsedVal > 0 && parsedVal < MODERN_ACTIVITY) {
+    let year = Math.log(MODERN_ACTIVITY / parsedVal) / (LN2 / HALF_LIFE_PERIOD);
+    
+    return Math.ceil(year);
+  } 
+  return false;
 };
